972fdc7ba58dcab709483c6654efa90a59a6ae41
super-jingjing <42860768+super-jingjing@users.noreply.github.com>
2019-05-29 10:58:36 +0800

    Add files via upload

---
+++ toolbar.js
@@ -0,0 +1,4 @@
+// const toolbar = ["searchreplace bold italic underline strikethrough alignleft aligncenter alignright outdent indent  blockquote undo redo removeformat subscript superscript code codesample", "hr bullist numlist link image charmap preview anchor pagebreak insertdatetime media table emoticons forecolor backcolor fullscreen"];
+const toolbar = ["searchreplace bold italic underline strikethrough alignleft aligncenter alignright outdent indent undo redo removeformat code", "hr bullist numlist link charmap preview anchor pagebreak table forecolor"];
+
+export default toolbar;
